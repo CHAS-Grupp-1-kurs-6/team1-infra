ea7a3d8c1c0de91518ec6c2459e6cb4f1ae42cda
SE-dotcomchas <samuel.engstrom@chasacademy.se>
2026-09-14 14:13:52 +0200

    test

--- main.tf
+++ main.tf
@@ -78,13 +78,14 @@ resource "google_compute_instance" "jumphost" {
   machine_type = "e2-micro"
   zone         = local.jumphost_zone
 
-  allow_stopping_for_update = true
-  can_ip_forward            = true
- 
-  service_account {
+    service_account {
     email  = "team${var.team_id}-jumphost@${var.project_id}.iam.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
+
+
+  allow_stopping_for_update = true
+  can_ip_forward            = true
 
  
   tags = ["jumphost"]
